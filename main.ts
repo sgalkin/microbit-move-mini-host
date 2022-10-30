b417f85dfb242d02fe583cc37f0f7c863231e352
Sergey Galkin <sgalkin@users.noreply.github.com>
2022-10-30 23:03:06 +0100

    handle light

--- main.ts
+++ main.ts
@@ -3,12 +3,15 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     setVelocity(90)
+    enableLight()
+})
+function enableLight () {
     strip.range(0, 1).showColor(neopixel.colors(NeoPixelColors.Red))
     strip.range(1, 1).showColor(neopixel.colors(NeoPixelColors.Orange))
     strip.range(2, 1).showColor(neopixel.colors(NeoPixelColors.Green))
     strip.range(3, 1).showColor(neopixel.colors(NeoPixelColors.Blue))
     strip.range(4, 1).showColor(neopixel.colors(NeoPixelColors.Violet))
-})
+}
 input.onButtonPressed(Button.AB, function () {
     setVelocity(0)
 })
@@ -18,10 +21,22 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 input.onButtonPressed(Button.B, function () {
+    setVelocity(-90)
+    disableLight()
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "light") {
+        if (0 == 0) {
+            disableLight()
+        } else {
+            enableLight()
+        }
+    }
+})
+function disableLight () {
     strip.clear()
     strip.show()
-    setVelocity(-90)
-})
+}
 function setVelocity (velocity: number) {
     velocity = velocity % 91
     pins.servoWritePin(AnalogPin.P1, 90 - velocity)
@@ -32,6 +47,7 @@ let strip: neopixel.Strip = null
 setVelocity(0)
 radio.setGroup(42)
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+disableLight()
 basic.forever(function () {
     basic.pause(200)
     strip.rotate(1)
